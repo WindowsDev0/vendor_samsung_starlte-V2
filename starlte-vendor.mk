eb005146f7a9439f098f7c626fee2800d035937b
Oliwier Nogaj <nogajoliwier@gmail.com>
2023-01-29 23:07:55 +0100

    starlte: remove dax-default (moved to device repo)

--- starlte-vendor.mk
+++ starlte-vendor.mk
@@ -55,7 +55,4 @@ PRODUCT_PACKAGES += \
 ifeq ($(TARGET_HAVE_SAMSUNG_DAP),true)
 PRODUCT_PACKAGES += \
     libswdap
-
-PRODUCT_COPY_FILES += \
-    vendor/samsung/starlte/proprietary/vendor/etc/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 endif
